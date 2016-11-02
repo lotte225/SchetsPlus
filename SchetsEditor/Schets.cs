ffb347a412e8a6ca1236f39667c8ce2ff336c0e1
lotte225 <lotte.van.horssen@outlook.com>
2016-11-02 13:25:14 +0100

    Add files via upload
    Assignment 1 complete: cirkels toegevoegd aan de buttons
    twee nieuwe kleuren toegevoegd
    Assignment 2 complete: Opslaan en openen toegevoegd

---
+++ SchetsEditor/Schets.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace SchetsEditor
+{
+    public class Schets
+    {
+        private Bitmap bitmap;
+        public Bitmap Bitmap//zorgt ervoor dat de bitmap bereikt kan worden vanuit andere klassen
+        {
+            get
+            {
+                return bitmap;
+            }
+        }
+        
+        public Schets()
+        {
+            bitmap = new Bitmap(1, 1);
+        }
+
+        public Schets(Bitmap bitje)
+        {
+            bitmap = bitje;
+        }
+        public Graphics BitmapGraphics
+        {
+            get { return Graphics.FromImage(bitmap); }
+        }
+        public void VeranderAfmeting(Size sz)
+        {
+            if (sz.Width > bitmap.Size.Width || sz.Height > bitmap.Size.Height)
+            {
+                Bitmap nieuw = new Bitmap( Math.Max(sz.Width,  bitmap.Size.Width)
+                                         , Math.Max(sz.Height, bitmap.Size.Height)
+                                         );
+                Graphics gr = Graphics.FromImage(nieuw);
+                gr.FillRectangle(Brushes.White, 0, 0, sz.Width, sz.Height);
+                gr.DrawImage(bitmap, 0, 0);
+                bitmap = nieuw;
+            }
+        }
+        public void Teken(Graphics gr)
+        {
+            gr.DrawImage(bitmap, 0, 0);
+        }
+        public void Schoon()
+        {
+            Graphics gr = Graphics.FromImage(bitmap);
+            gr.FillRectangle(Brushes.White, 0, 0, bitmap.Width, bitmap.Height);
+        }
+        public void Roteer()
+        {
+            bitmap.RotateFlip(RotateFlipType.Rotate90FlipNone);
+        }
+    }
+}
