b23509d01e33fd743bc9bcb6e8b7c97c3ec91fde
lotte225 <lotte.van.horssen@outlook.com>
2016-11-02 16:53:49 +0100

    Add files via upload
    overbodige comments en spacing weggehaald, 
    vierkantjes in comments gezet

--- SchetsEditor/Tools.cs
+++ SchetsEditor/Tools.cs
@@ -36,15 +36,15 @@ namespace SchetsEditor
         public override void Letter(SchetsControl s, char c)
         {
             if (c >= 32)
-            {
+            {  
                 Graphics gr = s.MaakBitmapGraphics();
                 Font font = new Font("Tahoma", 40);
                 string tekst = c.ToString();
                 SizeF sz = 
                 gr.MeasureString(tekst, font, this.startpunt, StringFormat.GenericTypographic);
-                gr.DrawString   (tekst, font, kwast, 
-                                              this.startpunt, StringFormat.GenericTypographic);
-                // gr.DrawRectangle(Pens.Black, startpunt.X, startpunt.Y, sz.Width, sz.Height);
+                gr.DrawString   (tekst, font, kwast, this.startpunt, StringFormat.GenericTypographic);
+                if (c == 32) sz.Width = 20; // zorgt dat de spatie getekend wordt!
+                //gr.DrawRectangle(Pens.Black, startpunt.X, startpunt.Y, sz.Width, sz.Height);
                 startpunt.X += (int)sz.Width;
                 s.Invalidate();
             }
